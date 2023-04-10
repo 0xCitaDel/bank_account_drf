bf9e4b824546414143f60047270186f186fa5437
0xCitaDele <dorivays@gamil.com>
2023-04-10 22:23:15 +0300

    create customer model + media settings

--- web/app/settings.py
+++ web/app/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,7 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     #Local
-    'src.users',
+    'src.users.apps.UsersConfig',
     'src.bank',
 ]
 
@@ -112,7 +113,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- web/src/bank/models.py
+++ web/src/bank/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from app import settings
+import uuid
+import os
 
-# Create your models here.
+def customer_image_file_path(instance, filename):
+    """Generate file path for new image"""
+    ext = filename.split('.')[-1]
+    filename = f'{uuid.uuid4()}.{ext}'
+
+    return os.path.join(f'upload/{instance.user.id}/', filename)
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    house = models.CharField(max_length=255)
+    image = models.ImageField(null=True, upload_to=customer_image_file_path)
+
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
